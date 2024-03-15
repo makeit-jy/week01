df45989e0331558b391e6cce5f4ccefb58d41d7f
makeit-jy <64471645+makeit-jy@users.noreply.github.com>
2024-03-15 21:47:13 +0900

    [Bronze V] Title: 사칙연산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,23 @@
+a,b = input().split()
+a = int(a)
+b = int(b)
+print("%d\n%d\n%d\n%d\n%d"%(a+b, a-b, a*b, a/b, a%b))
+
+#with open('input.txt', 'r') as f:
+#   a, b = map(int, f.readline().split())
+
+#a = int(a)
+#b = int(b)
+
+#sum = a+b
+#sub = a-b
+#mul = a*b
+#div = a//b
+#remain = a%b
+
+#with open('output.txt', 'w') as f:
+#    f.write(f"{sum}\n")
+#    f.write(f"{sub}\n")
+#    f.write(f"{mul}\n")
+#    f.write(f"{div}\n")
+#    f.write(f"{remain}\n")
