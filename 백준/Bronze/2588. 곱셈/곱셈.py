c91bb18796371f9235c395ecd07cc51bc4357e1a
makeit-jy <64471645+makeit-jy@users.noreply.github.com>
2024-03-15 22:49:49 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,7 +1,7 @@
 a = int(input())
-b = int(input())
+b = input()
 
-print(a * (b % 10))
-print(a * (b % 100 // 10))
-print(a * (b // 100))
-print(a * b)
+print( a * int(b[2]))
+print( a * int(b[1]))
+print( a * int(b[0]))
+print( a * int(b))
