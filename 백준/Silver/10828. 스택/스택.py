548a76d9e53abed037828327333a047f43e062c2
makeit-jy <64471645+makeit-jy@users.noreply.github.com>
2024-03-21 19:40:25 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,20 +1,42 @@
 import sys 
-n = int(sys.stdin.readline())
+cnt = int(sys.stdin.readline())
 
 stack = []
 
-for _ in range(n):
-    s = sys.stdin.readline().split() 
-    if s[0] == 'push':
-        stack.append(s[1])
-    elif s[0] == 'pop':
+for _ in range(cnt):
+    word = sys.stdin.readline().split() 
+    if word[0] == 'push':
+        stack.append(word[1])
+    elif word[0] == 'pop':
         if len(stack)>0: print(stack.pop())
         else: print(-1)
-    elif s[0] == 'size':
+    elif word[0] == 'size':
         print(len(stack))
-    elif s[0] == 'empty':
+    elif word[0] == 'empty':
         if len(stack)==0: print(1)
         else: print(0)
-    elif s[0] == 'top':
+    elif word[0] == 'top':
         if len(stack)>0: print(stack[-1])
         else: print(-1)
+
+# cnt = int(input())
+# stack = []
+
+# for _ in range(cnt):
+#     word = input().split()
+#     if word[0] == 'push':
+#         stack.append(word[1])
+#     elif word[0] == 'pop':
+#         if len(stack) > 0: 
+#             print(stack.pop())
+#         else: print(-1)
+#     elif word[0] == 'size':
+#         print(len(stack))
+#     elif word[0] == 'empty':
+#         if len(stack)==0: 
+#             print(1)
+#         else: print(0)
+#     elif word[0] == 'top':
+#         if len(stack)>0:
+#             print(stack[-1])
+#         else: print(-1)
