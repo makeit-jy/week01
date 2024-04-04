59a45a6a6c68b54f8cbd508e4ef8e516f6b42b39
makeit-jy <64471645+makeit-jy@users.noreply.github.com>
2024-04-04 09:53:59 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+cnt = 0
+ 
+while True:
+  if n % 5 != 0:
+    n = n - 3
+    cnt += 1
+ 
+  elif n % 5 == 0:
+    cnt += n // 5
+    print(cnt)
+    break
+ 
+  if n < 0:
+    print(-1)
+    break
